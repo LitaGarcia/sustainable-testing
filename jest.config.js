45f63d2fd655b3f3bde0a05db2dda00cf9d0f2dd
Manuela García Yebra <manuelagarciayebra@gmail.com>
2024-05-02 16:36:42 +0200

    feat: Added jest

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  //the module who executes the tests
+  preset: 'ts-jest',
+  //could be jsdom for example
+  testEnvironment: 'node',
+};
