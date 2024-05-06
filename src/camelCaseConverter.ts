ddb1874ea44de097bd244424ddb182b52c02a580
Manuela García Yebra <manuelagarciayebra@gmail.com>
2024-05-06 12:52:54 +0200

    feat: third implementation based on course

--- src/camelCaseConverter.ts
+++ src/camelCaseConverter.ts
@@ -1,8 +1,4 @@
 export function camelCaseConverter(text: string): string {
-    const regexExp: RegExp = /\s+|[-_]/g;
-
-    const convertedText: string = text.replace(regexExp, '#')
-    return convertedText.split('#').map((word: string) => {
-        return word.charAt(0).toUpperCase() + word.slice(1)
-    }).join('')
+    const words = text.split(/[ _-]/);
+    return words.map(word => word.charAt(0).toUpperCase() + word.substr(1)).join('');
 }
