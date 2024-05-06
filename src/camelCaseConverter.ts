c3c6759fd655c3dadede2027883c7a51c6632b7e
Manuela García Yebra <manuelagarciayebra@gmail.com>
2024-05-06 12:15:42 +0200

    feat: second implementation

--- src/camelCaseConverter.ts
+++ src/camelCaseConverter.ts
@@ -1,41 +1,8 @@
 export function camelCaseConverter(text: string): string {
-    const regex = /(?:^|\s+)(\w+)(?:[_-]+)(\w+)/g
+    const regexExp: RegExp = /\s+|[-_]/g;
 
-    if (text.match(regex)) {
-        const textList: string[][] = text.split(' ').map((word: string) => word.split(/[_-]/));
-        const replacedTextList = textList.map((word: string[]) => {
-            word.join('')
-            return word.map((w: string) => {
-                return w.charAt(0).toUpperCase() + w.slice(1)
-            }).join('').toString()
-        })
-        return replacedTextList.join('')
-    } else if (emptyStringValidator(text) || stringUpperCaseValidator(text)) {
-        return text
-    } else if (stringSpaceValidator(text)) {
-        return text.split(' ').join('')
-    } else if (stringCamelCaseValidator(text)) {
-        return text.charAt(0).toUpperCase() + text.slice(1)
-    }
-
+    const convertedText: string = text.replace(regexExp, '#')
+    return convertedText.split('#').map((word: string) => {
+        return word.charAt(0).toUpperCase() + word.slice(1)
+    }).join('')
 }
-
-function emptyStringValidator(text: string): boolean {
-    return text === ''
-}
-
-function stringUpperCaseValidator(text: string): boolean {
-    const upperCaseRegex: RegExp = /^[A-Z][a-zA-Z]*$/
-    return upperCaseRegex.test(text)
-}
-
-function stringSpaceValidator(text: string): boolean {
-    const spaceRegex: RegExp = /\s/
-    return spaceRegex.test(text)
-}
-
-function stringCamelCaseValidator(text: string) {
-    const camelCaseRegex: RegExp = /^[a-z]/
-    return camelCaseRegex.test(text)
-}
-
