99a4cc09a80cfc2d9696c4dd8334c022aaf5a498
Manuela García Yebra <manuelagarciayebra@gmail.com>
2024-05-06 13:50:04 +0200

    feat: create builder pattern

--- src/tests/diseaseFilter.test.ts
+++ src/tests/diseaseFilter.test.ts
@@ -4,27 +4,23 @@ describe('Disease filter', () => {
     it('filters cases when several diagnosis filters are applied together', () => {
         const searchCriterion1 = 'Vías Respiratorias Altas';
         const searchCriterion2 = 'Cerebro';
-        const diagnoses = [
-            createDiagnosis(1, searchCriterion1),
-            createDiagnosis(2, searchCriterion2),
-            createDiagnosis(3, 'Irrelevant-location'),
-        ];
         const expectedName1 = 'Chupito';
         const expectedName2 = 'Juliana';
-        const cases = [
-            createCase(1, expectedName1),
-            createCase(2, expectedName2),
-            createCase(3, 'Irrelevant-name'),
-        ];
-        const diseaseFilter = DiseaseFilter.create(cases, diagnoses);
+        const fixtures = casesWithDiagnoses()
+            .havingDiagnosisWithLocationAndCaseWithName(searchCriterion1, expectedName1)
+            .havingDiagnosisWithLocationAndCaseWithName(searchCriterion2, expectedName2)
+            .havingDiagnosisWithLocationAndCaseWithName('Irrelevant-location', 'Irrelevant-name')
+            .build();
+
+        const diseaseFilter = DiseaseFilter.create(fixtures.cases(), fixtures.diagnoses());
         diseaseFilter.addFilter(searchCriterion2);
         diseaseFilter.addFilter(searchCriterion1);
 
         const result = diseaseFilter.casesFiltered;
 
         expect(result.length).toBe(2);
-        expect(result[1].patientName).toBe('Chupito');
-        expect(result[0].patientName).toBe('Juliana');
+        expect(result[1].patientName).toBe(expectedName1);
+        expect(result[0].patientName).toBe(expectedName2);
     });
 });
 
@@ -48,4 +44,29 @@ function createDiagnosis(id: number, location: string): Diagnosis {
         origin: 'irrelevant-origin',
         specie: 'irrelevant-specie',
     };
+}
+
+function casesWithDiagnoses() {
+    let diagnosisId = 0;
+    let diagnoses: Diagnosis[] = [];
+    let cases: Case[] = [];
+
+    const add = (location: string, patientName: string) => {
+        diagnosisId++;
+        diagnoses.push(createDiagnosis(diagnosisId, location));
+        cases.push(createCase(diagnosisId, patientName));
+    };
+
+    const builder = {
+        havingDiagnosisWithLocationAndCaseWithName: (location: string, patientName: string) => {
+            add(location, patientName);
+            return builder;
+        },
+        build: () => ({
+            cases: () => cases,
+            diagnoses: () => diagnoses,
+        }),
+    };
+
+    return builder;
 }
