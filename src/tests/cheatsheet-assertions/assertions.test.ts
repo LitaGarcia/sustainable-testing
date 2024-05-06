904195f358d4b2e58a11de9bbfa0d8038a4ad308
Manuela García Yebra <manuelagarciayebra@gmail.com>
2024-05-06 15:17:02 +0200

    feat: create custom assertion extending

--- src/tests/cheatsheet-assertions/assertions.test.ts
+++ src/tests/cheatsheet-assertions/assertions.test.ts
@@ -83,3 +83,36 @@ function listMatchers<T>(list: T[]) {
         },
     };
 }
+
+
+//custom assertions (extending) - we can extend the jest matchers
+declare namespace jest {
+    interface Matchers<R> {
+        customToBe(value): CustomMatcherResult;
+
+        isExactly<T>(...values: T[]): CustomMatcherResult;
+    }
+}
+
+expect.extend({
+    customToBe(expected, received) {
+        return {
+            pass: expected === received,
+            message: () => `Expected: ${expected} \nReceived: ${received}`,
+        };
+    },
+    isExactly<T>(expectedList: T[], ...values: T[]) {
+        const haveSameLength = expectedList.length === values.length;
+        const haveSameElements = () =>
+            values.map((_, i) => values[i] === expectedList[i]).reduce((p, c) => p && c, true);
+        return {
+            pass: haveSameLength && haveSameElements(),
+            message: () => `Expected: ${expectedList} \nReceived: ${values}`
+        };
+    }
+});
+
+test('custom assertion (extending)', () => {
+    const list = [10, 20, 30];
+    expect(list).isExactly(10, 20, 30);
+});
