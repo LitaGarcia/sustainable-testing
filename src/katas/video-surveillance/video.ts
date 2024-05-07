f3d4b3f8e1327127bc67aa2efe62d09803305a1e
Manuela García Yebra <manuelagarciayebra@gmail.com>
2024-05-07 13:12:01 +0200

    fix: better var naming

--- src/katas/video-surveillance/video.ts
+++ src/katas/video-surveillance/video.ts
@@ -1,14 +1,14 @@
 import {MotionSensor} from "./interfaces";
 
 export class VideoRecorder implements VideoRecorder {
-    recorder: boolean;
+    isRecording: boolean;
 
     constructor(private readonly motionSensor: MotionSensor) {
     }
 
     startRecording(): void {
         if (!this.motionSensor.isDetectingMotion()) {
-            this.recorder = false;
+            this.isRecording = false;
         }
 
     }
