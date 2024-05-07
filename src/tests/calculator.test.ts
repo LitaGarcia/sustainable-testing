e3427a15fcb6cacf9d7684f3fc7ae768afbe24a0
Manuela García Yebra <manuelagarciayebra@gmail.com>
2024-05-07 10:40:16 +0200

    feat: add mock and stub with jest spyOn

--- src/tests/calculator.test.ts
+++ src/tests/calculator.test.ts
@@ -1,7 +1,7 @@
 import * as arithmetic from "../core/calculator/arithmetic";
 import * as calculator from "../core/calculator/calculator";
 
-describe('The calculator', () => {
+describe.skip('The calculator', () => {
     // this is a spy + stub
     (arithmetic as any).add = jest.fn(arithmetic.add);
     (arithmetic as any).subtract = jest.fn(arithmetic.subtract);
@@ -32,5 +32,37 @@ describe.skip('The calculator', () => {
     it("calls arithmetic.subtract", () => {
         calculator.doSubtract(1, 2);
         expect(arithmetic.subtract).toHaveBeenCalledWith(1, 2);
+    });
+});
+
+//best way! mantean the original implementation and type checking
+// this is a real mock
+describe('The calculator', () => {
+    it("calls arithmetic add", () => {
+        const mockedAdd = jest.spyOn(arithmetic, 'add');
+        expect(calculator.doAdd(1, 2)).toBe(3);
+        expect(mockedAdd).toHaveBeenCalledWith(1, 2);
+    });
+    it("calls arithmetic subtract", () => {
+        const mockedSubtract = jest.spyOn(arithmetic, 'subtract');
+        expect(calculator.doSubtract(1, 2)).toBe(-1);
+        expect(mockedSubtract).toHaveBeenCalledWith(1, 2);
+    });
+});
+
+// we can use it also to stub the implementation
+
+describe('The calculator', () => {
+    //this will fail because we are returning the force value 10
+    xit("calls arithmetic add", () => {
+        const mockedAdd = jest.spyOn(arithmetic, 'add');
+        mockedAdd.mockImplementation(() => 10);
+        expect(calculator.doAdd(1, 2)).toBe(3);
+        expect(mockedAdd).toHaveBeenCalledWith(1, 2);
+    });
+    it("calls arithmetic subtract", () => {
+        const mockedSubtract = jest.spyOn(arithmetic, 'subtract');
+        expect(calculator.doSubtract(1, 2)).toBe(-1);
+        expect(mockedSubtract).toHaveBeenCalledWith(1, 2);
     });
 });
