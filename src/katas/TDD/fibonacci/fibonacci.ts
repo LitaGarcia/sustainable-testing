e005be8aa616f22684cd56ea109d0b81b425cc62
Lita García <manuelagarciayebra@gmail.com>
2024-05-13 16:03:16 +0200

    feat: add recursive function solution to fibonacci sequence

--- src/katas/TDD/fibonacci/fibonacci.ts
+++ src/katas/TDD/fibonacci/fibonacci.ts
@@ -9,3 +9,11 @@ export function fibonacciSequence(limit: number): number  {
 
     return sequence[limit - 1]
 }
+
+
+//solution with recursive function
+export function fibonacci(number: number) {
+    if (number === 0) return 0;
+    if (number <= 2) return 1;
+    return fibonacci(number - 1) + fibonacci(number - 2);
+}
--- src/tests/katas/TDD/fibonacci.test.ts
+++ src/tests/katas/TDD/fibonacci.test.ts
@@ -1,4 +1,4 @@
-import {fibonacciSequence} from "../../../katas/TDD/fibonacci/fibonacci";
+import {fibonacci, fibonacciSequence} from "../../../katas/TDD/fibonacci/fibonacci";
 
 describe('The fibonacci sequence', () => {
         it('should return the number of the position desired', () => {
@@ -6,3 +6,18 @@ describe('The fibonacci sequence', () => {
         })
         }
 )
+
+describe('The fibonacci sequence', () => {
+    it('yields value zero to number zero', () => {
+        expect(fibonacci(0)).toBe(0);
+    });
+
+    it('yields value one to number one', () => {
+        expect(fibonacci(1)).toBe(1);
+    });
+
+    it('is a series where the value for a number is the addition of the preceding two values', () => {
+        [2, 3, 4, 5].forEach((n) =>
+            expect(fibonacci(n)).toBe(fibonacci(n - 1) + fibonacci(n - 2)))
+    });
+});
